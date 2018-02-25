2e9324a5f315a5cb9f00540f499f689fab41bccf
Eugene Chernenko <chernenko@gmail.com>
2018-02-25 20:13:41 +0000

    added babbel and webpack configs

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'public/js');
+var APP_DIR = path.resolve(__dirname, 'src/js');
+
+var config = {
+    entry: APP_DIR + '/form.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                include: APP_DIR,
+                use: ['babel-loader']
+            }
+        ]
+    }
+};
+
+module.exports = config;
